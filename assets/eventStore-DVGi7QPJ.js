0b90badc30a6194ef646c423a2d03ac554d1423e
Begeta-ice <Begeta-ice@users.noreply.github.com>
2024-10-28 09:27:32 +0000

    deploy: 020aa1a6d0ba9b1577d091056de7fa1db7454a0e

---
+++ assets/eventStore-DVGi7QPJ.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-BolfWz0k.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvent(){return this.events}}});export{n as u};
+//# sourceMappingURL=eventStore-DVGi7QPJ.js.map
