d7e31dd1f99280a2112afedaa2641e55c9cc3638
Begeta-ice <Begeta-ice@users.noreply.github.com>
2024-10-28 09:45:10 +0000

    deploy: 0baba24401a57d6c78ff84a6ea6010d0b3e5aea7

--- assets/eventStore-DVGi7QPJ.js
+++
@@ -1,2 +0,0 @@
-import{g as t}from"./index-BolfWz0k.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvent(){return this.events}}});export{n as u};
-//# sourceMappingURL=eventStore-DVGi7QPJ.js.map
---
+++ assets/eventStore-nZ7UkX4R.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-DU6RKldD.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvent(){return this.events}}});export{n as u};
+//# sourceMappingURL=eventStore-nZ7UkX4R.js.map
