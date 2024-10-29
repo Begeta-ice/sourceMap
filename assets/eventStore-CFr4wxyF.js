507a0e62cdcbd6f9a5090d25a5d3336fd31eeabc
Begeta-ice <Begeta-ice@users.noreply.github.com>
2024-10-29 08:07:16 +0000

    deploy: 96a85602f5af0c2fbfbfde4ce47dad06440569cf

---
+++ assets/eventStore-CFr4wxyF.js
@@ -0,0 +1,2 @@
+import{g as t}from"./index-CXhvSffN.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvent(){return this.events}}});export{n as u};
+//# sourceMappingURL=eventStore-CFr4wxyF.js.map
--- assets/eventStore-nZ7UkX4R.js
+++
@@ -1,2 +0,0 @@
-import{g as t}from"./index-DU6RKldD.js";const n=t("eventStore",{state:()=>({events:[]}),getters:{},actions:{setEvents(e){this.events=e},getEvent(){return this.events}}});export{n as u};
-//# sourceMappingURL=eventStore-nZ7UkX4R.js.map
